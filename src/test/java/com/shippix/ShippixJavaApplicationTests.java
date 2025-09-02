833d4ea39b3f07335517340ad02e89b89ea99dcd
Noureldin Shimi <nouredinshimi@gmail.com>
2025-09-02 16:08:14 +0300

    Initial commit - Spring Boot project

---
+++ src/test/java/com/shippix/ShippixJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shippix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippixJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
