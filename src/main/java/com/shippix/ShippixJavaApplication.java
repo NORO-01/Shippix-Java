4b6b8ba5615c5b9144840063e401f77f5b45e571
mayafouad <11410120220270@stud.cu.edu.eg>
2025-10-02 17:47:34 +0300

    add BO

--- src/main/java/com/shippix/ShippixJavaApplication.java
+++ src/main/java/com/shippix/ShippixJavaApplication.java
@@ -1,5 +1,7 @@
 package com.shippix;
 
+import com.shippix.User_Management.Model.BusinessOwner;
+import com.shippix.User_Management.Model.BusinessType;
 import com.shippix.User_Management.Model.Users;
 import com.shippix.User_Management.Repo.UserRepo;
 import org.springframework.boot.CommandLineRunner;
@@ -34,6 +36,19 @@ public class ShippixJavaApplication {
 				admin.setRole(Users.Role.ROLE_ADMIN);
 				userRepository.save(admin);
 				System.out.println("Admin user updated: admin1 / admin123");
+			}
+			if(userRepository.findByUsername("owner1").isEmpty()) {
+				BusinessOwner owner = new BusinessOwner();
+				owner.setUsername("owner1");
+				owner.setPassword(passwordEncoder.encode("owner123"));
+				owner.setEmail("owner1@example.com");
+				owner.setRole(Users.Role.ROLE_BUSINESS_OWNER);
+				owner.setBusinessName("Tech Store");
+				owner.setBusinessType(BusinessType.ELECTRONICS_STORE);
+				owner.setLatitude(30.0444); // Cairo example
+				owner.setLongitude(31.2357); 
+				userRepository.save(owner);
+				System.out.println("Business owner created: " + owner.getUsername() + " / owner123");
 			}
 		};
 	}
